1438c724e9f4ceb2974218c73cd2902fadc68ba0
Rodrigo Lopes <rodrigo.lopes@olx.com>
2017-10-23 10:52:55 +0100

    optional header added

--- client.go
+++ client.go
@@ -50,8 +50,10 @@ type Fetch struct {
 }
 
 func (f *Fetch) Do(req *http.Request) (*Response, error) {
-	if f.opt != nil
-	req.Header = f.opt.Header
+	if f.opt.Header != nil {
+		req.Header = f.opt.Header
+	}
+
 	resp, err := f.Client.Do(req)
 	return &Response{Response: resp}, err
 }
